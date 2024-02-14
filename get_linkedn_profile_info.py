373f3c2c854d7434de3ecb15083233f2cf34f99c
Jay M Gonzalez <jaimungon@gmail.com>
2024-02-14 09:18:50 +0100

    add models

--- get_linkedn_profile_info.py
+++ get_linkedn_profile_info.py
@@ -2,16 +2,48 @@ import requests
 import dotenv
 import os
 import json
+import re
 import pandas as pd
 from openai import OpenAI
 import instructor
-import pydantic
+from pydantic import BaseModel, Field
+from typing import List
+
+
+# models.py
+class Activity(BaseModel):
+    """Represents a LinkedIn activity."""
+
+    title: str
+
+
+class Experience(BaseModel):
+    """Represents a LinkedIn experience."""
+
+    title: str
+    company: str
+    date_range: str
+    location: str
+    description: str
+
+
+class LinkedInProfile(BaseModel):
+    name: str
+    headline: str
+    summary: str
+    location: str
+    industry: str
+    education: List[str]
+    experience: List[Experience] = Field(max_items=3)
+    activities: List[Activity]
+    followers: int
+
 
 dotenv.load_dotenv()
 token = os.environ.get("CRAWLBASE_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-client = OpenAI(api_key=OPENAI_API_KEY)
+client = instructor.patch(OpenAI(api_key=OPENAI_API_KEY))
 
 
 def get_urls(df, column_name):
@@ -106,6 +138,30 @@ def check_if_exists(index, df, column_name):
     return pd.notna(df.at[index[0], column_name])
 
 
+def remove_links_and_specific_key(d, target_key="peopleAlsoViewed"):
+    url_pattern = re.compile(r"https?://\S+|www\.\S+")
+    for key in list(
+        d.keys()
+    ):  # Use list(d.keys()) to avoid RuntimeError during iteration
+        if key == target_key:
+            del d[key]  # Remove the specific key
+            continue  # Skip further checks and continue with the next iteration
+        if isinstance(d[key], dict):
+            d[key] = remove_links_and_specific_key(d[key], target_key)
+        elif isinstance(d[key], list):
+            d[key] = [
+                (
+                    remove_links_and_specific_key(item, target_key)
+                    if isinstance(item, dict)
+                    else item
+                )
+                for item in d[key]
+            ]
+        elif isinstance(d[key], str) and url_pattern.search(d[key]):
+            del d[key]  # Remove key if value is a URL
+    return d
+
+
 def append_item_to_row(df, item, id, column_id, column_name):
     index = df[df[column_id] == id].index
     if not index.empty:
@@ -118,10 +174,11 @@ def synthesize_profile(profile):
     try:
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
+            response_model=LinkedInProfile,
             messages=[
                 {
                     "role": "system",
-                    "content": "Given a JSON representation of a LinkedIn profile, extract and synthesize the most relevant information to be used in crafting a personalized connection request message. Focus on key areas such as current position, company, industry, and recent activity. Ensure the output is concise, highlighting only the information that could make a connection request more personalized and engaging.",
+                    "content": "Given a JSON representation of a LinkedIn profile, extract and synthesize the most relevant information to be used in crafting a personalized connection request message\nhighlighting the information that could make a connection request more personalized and engaging\nmake sure to add the recent posts if any in activities\ndo not include http links",
                 },
                 {
                     "role": "user",
@@ -130,7 +187,7 @@ def synthesize_profile(profile):
             ],
         )
         print(response)
-        return response.choices[0].message.content
+        return response.model_dump_json(indent=2)
 
     except Exception as e:
         print(e)
@@ -170,7 +227,14 @@ def update_csv_with_profile(csv_name, column_name="rid"):
         if profile is None:
             continue
 
+        profile = json.loads(profile)
+        # print(type(profile))
+
         df = append_item_to_row(df, profile, id, column_name, "linkedinProfile")
+
+        profile = remove_links_and_specific_key(profile)
+
+        profile = json.dumps(profile)
 
         df = append_item_to_row(
             df, synthesize_profile(profile), id, column_name, "synthesizedProfile"
