4c3c1eaee4fba43242ec7eb76e9c2d3ce41b3244
Jay M Gonzalez <jaimungon@gmail.com>
2024-02-23 10:47:48 +0100

    finish scrapreComments

--- scrapeComments.js
+++ scrapeComments.js
@@ -25,36 +25,67 @@ function downloadCSV(csv, filename) {
   downloadLink.click()
 }
 
+async function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time)
+  })
+}
+
 // Function to load more comments
-function loadMoreComments() {
-  var loadMoreCommentsButton = document.querySelector(
-    '.comments-comments-list__load-more-comments-button'
-  )
-  for (let i = 0; i < 5; i++) {
-    simulateMouseScrollToEnd()
+async function loadMoreComments() {
+  for (let i = 0; i < 3; i++) {
+    var loadMoreCommentsButton = document.querySelector(
+      '.comments-comments-list__load-more-comments-button'
+    )
+    await simulateMouseScrollToEnd()
     loadMoreCommentsButton.click()
-    setTimeout(() => {
-      console.log('Loading more comments...')
-    }, 3000)
+    console.log('Clicked to load more comments...')
+    await delay(5000)
+    console.log('Loading more comments...')
   }
+
+  console.log('Finished loading comments.')
 }
 
 function easeOutQuad(t) {
   return t * (2 - t)
 }
 
-function simulateMouseScrollToEnd(duration = 2000) {
-  let start = window.pageYOffset
-  let end = document.body.offsetHeight - window.innerHeight
-  let change = end - start
+async function simulateMouseScrollToEnd(duration = 1500, maxDuration = 30000) {
+  let start = window.scrollY
   let currentTime = 0
-  const increment = 20
+  const increment = 50
+  let timeoutReached = false
+
+  // Set a timeout to mark when 30 seconds have passed
+  setTimeout(() => {
+    timeoutReached = true
+  }, maxDuration)
 
   function animateScroll() {
+    if (timeoutReached) {
+      return
+    }
+
+    let documentHeight = Math.max(
+      document.body.scrollHeight,
+      document.documentElement.scrollHeight,
+      document.documentElement.offsetHeight
+    )
+    let windowHeight = window.innerHeight
+    let destination = documentHeight - windowHeight
+    let change = destination - start
+
     currentTime += increment
     let val = easeOutQuad(currentTime / duration)
     window.scrollTo(0, start + change * val)
-    if (currentTime < duration) {
+
+    if (window.scrollY < destination && currentTime < duration) {
+      setTimeout(animateScroll, increment)
+    } else if (window.scrollY < destination) {
+      // If we haven't reached the new bottom, extend the duration slightly
+      currentTime = 0 // Reset currentTime to allow the scroll to adjust to new content
+      start = window.scrollY // Update start to current position
       setTimeout(animateScroll, increment)
     }
   }
@@ -62,32 +93,62 @@ function simulateMouseScrollToEnd(duration = 2000) {
   animateScroll()
 }
 
-function extractDataAndDownloadCSV() {
-  loadMoreComments()
+function getUserName() {
+  return document.querySelector(
+    '.update-components-actor__container .update-components-actor__name .visually-hidden'
+  ).innerText
+}
 
-  var usernames = document.querySelectorAll('.username') // Placeholder selector
-  var userUrls = document.querySelectorAll('.userUrl') // Placeholder selector
-  var comments = document.querySelectorAll('.comment') // Placeholder selector
+function getHook() {
+  return document
+    .querySelector('.feed-shared-update-v2__description-wrapper')
+    .innerText.split('\n')[0]
+    .replaceAll(' ', '-')
+}
+
+async function extractDataAndDownloadCSV() {
+  await loadMoreComments()
+
+  var post = document.querySelector(
+    '.feed-shared-update-v2__description-wrapper'
+  ).innerText
+
+  var commentContainer = document.querySelector(
+    '.feed-shared-update-v2__comments-container'
+  )
+
+  var comments = commentContainer.querySelectorAll(
+    '.comments-comment-item.comments-comments-list__comment-item'
+  )
+
   var csv = []
-
   // Header row
-  csv.push('username,userUrl,comment')
+  csv.push('fullName,n,c')
 
-  // Data rows
-  for (let i = 0; i < usernames.length; i++) {
-    var username = usernames[i].innerText || usernames[i].textContent
-    var userUrl = userUrls[i].getAttribute('href')
-    var comment = comments[i].innerText || comments[i].textContent
+  // for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < comments.length; i++) {
+    var username = comments[i]
+      .querySelectorAll('.comments-post-meta a')[1]
+      .innerText.split('\n')[0]
+    var commentText = comments[i].querySelector(
+      '.comments-comment-item-content-body'
+    ).innerText
 
     // Escape commas and double quotes from the comment
-    comment = comment.replace(/"/g, '""')
-    if (comment.includes(',')) comment = `"${comment}"`
+    commentText = commentText.replace(/,/g, ';')
+    commentText = commentText.replace(/\n/g, ' ')
+    commentText = commentText.replace(/"/g, '')
+    commentText = commentText.replace(getUserName(), '')
 
-    csv.push(`${username},${userUrl},${comment}`)
+    username = username.replace(/,/g, '')
+
+    csv.push(`${username},${username.split(' ')[0]},${commentText}`)
   }
+  // csv.push(post)
 
+  var filename = `${getUserName()}_${getHook()}`
   // Download CSV
-  downloadCSV(csv.join('\n'), 'data.csv')
+  downloadCSV(csv.join('\n'), `${filename}.csv`)
 }
 
 // Run the function to extract data and download it as a CSV
